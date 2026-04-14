452a537dd5bf3c25746c5104cf0aafb3eef90950
Ofure Akpengbe <imonakpengbe@gmail.com>
2026-04-14 13:29:06 +0100

    fix vercel deployment structure

--- api/classify.py
+++ api/classify.py
@@ -3,47 +3,38 @@ from datetime import datetime, timezone
 
 def handler(request):
 
-    # Get query param
     name = request.query_params.get("name")
 
-    # Validation
     if not name or name.strip() == "":
         return {
             "status": "error",
             "message": "Name query parameter is required"
         }
 
-    try:
-        r = httpx.get("https://api.genderize.io", params={"name": name})
-        data = r.json()
+    r = httpx.get("https://api.genderize.io", params={"name": name})
+    data = r.json()
 
-        gender = data.get("gender")
-        probability = data.get("probability")
-        count = data.get("count")
+    gender = data.get("gender")
+    probability = data.get("probability")
+    count = data.get("count")
 
-        if gender is None or count == 0:
-            return {
-                "status": "error",
-                "message": "No prediction available for the provided name"
-            }
-
-        sample_size = count
-        is_confident = probability >= 0.7 and sample_size >= 100
-
-        return {
-            "status": "success",
-            "data": {
-                "name": name,
-                "gender": gender,
-                "probability": probability,
-                "sample_size": sample_size,
-                "is_confident": is_confident,
-                "processed_at": datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
-            }
-        }
-
-    except Exception:
+    if gender is None or count == 0:
         return {
             "status": "error",
-            "message": "External API failure"
+            "message": "No prediction available for the provided name"
         }
+
+    sample_size = count
+    is_confident = probability >= 0.7 and sample_size >= 100
+
+    return {
+        "status": "success",
+        "data": {
+            "name": name,
+            "gender": gender,
+            "probability": probability,
+            "sample_size": sample_size,
+            "is_confident": is_confident,
+            "processed_at": datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
+        }
+    }
